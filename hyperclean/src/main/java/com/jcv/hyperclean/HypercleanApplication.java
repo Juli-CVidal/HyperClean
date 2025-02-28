6836ff3ed4f0275929a88ed1d46e318a55c0dff9
Juli-CVidal <juli.vidal.019@gmail.com>
2025-02-28 19:52:57 -0300

    [WIP] First commit - Defining basic structure and connections

---
+++ hyperclean/src/main/java/com/jcv/hyperclean/HypercleanApplication.java
@@ -0,0 +1,13 @@
+package com.jcv.hyperclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypercleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypercleanApplication.class, args);
+	}
+
+}
---
+++ hyperclean/src/test/java/com/jcv/hyperclean/HypercleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcv.hyperclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypercleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
