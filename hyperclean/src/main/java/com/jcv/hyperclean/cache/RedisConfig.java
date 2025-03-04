8dffe6d4361a98b5118585388ffe3da339646059
Juli-CVidal <juli.vidal.019@gmail.com>
2025-03-04 10:17:33 -0300

    redis - updated connection

--- hyperclean/src/main/java/com/jcv/hyperclean/cache/RedisConfig.java
+++ hyperclean/src/main/java/com/jcv/hyperclean/cache/RedisConfig.java
@@ -1,8 +1,5 @@
 package com.jcv.hyperclean.cache;
 
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,23 +11,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisClient redisClient(@Value("${spring.data.redis.host}") String redisHost,
-                                   @Value("${spring.data.redis.port}") int redisPort) {
-        RedisURI redisURI = RedisURI.create("redis://" + redisHost + ":" + redisPort);
-        return RedisClient.create(redisURI);
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
-
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
         template.afterPropertiesSet();
         return template;
     }
